b767bf464ee7361702face9d368c3d3e6187b257
amin007 <helmi007@yahoo.com>
2017-10-01 14:52:49 +0800

    susun balik koding

--- Aplikasi/Papar/pelajar/pendaftaran.php
+++ Aplikasi/Papar/pelajar/pendaftaran.php
@@ -11,29 +11,21 @@ $panggilFormat = new \Aplikasi\Kitab\HTML();
 	<div class="col-sm-8">
 		<div class="input-group input-group-lg">
 		<span class="input-group-addon">Contoh Borang Bersama Input</span>
-		</div>	
+		</div>
 	</div>
 </div>
-<?php
-foreach ($this->medanbaru as $namaMedan => $senarai):
-?><div class="form-group">
+<?php foreach ($this->medanbaru as $namaMedan => $senarai): ?><div class="form-group">
 	<label for="inputTajuk" class="col-sm-2 control-label"><?php echo $senarai['nama_label'] ?></label>
 	<div class="col-sm-6">
 <!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ --><?php
-	/*echo $namaMedan . '<br>'
-	.	$senarai['jenis_medan'] . '<br>'
-	.	$senarai['jenis_data'] . '<br>'
-	.	'';/*/
 	$tab = "\n\t\t";
 	echo $tab . $panggilFormat->tambahIkutType($this->jadual,$namaMedan,$senarai);
 	echo "\n";
 ?><!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
 	</div><!-- / class="col-sm-6" -->
 </div><!-- / class="form-group" -->
-<?php
-endforeach;
-?>
+<?php endforeach; ?>
 <!-- tamat - input tengah ------------------------------------------------------------------------------------------- -->
-</form>	
-<?php 
+</form>
+<?php
 include 'dibawah.php';
