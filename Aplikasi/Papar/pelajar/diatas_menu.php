a2d3ef2586a9d948b2cc4218b5f51ac2954119fb
amin007 <helmi007@yahoo.com>
2017-10-01 17:19:49 +0800

    tambah menu kecil

---
+++ Aplikasi/Papar/pelajar/diatas_menu.php
@@ -0,0 +1,31 @@
+<!-- ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+</ul -->
+<ul class="nav nav-pills">
+<li><a href="#">
+	<i class="fa fa-file-o fa-2x" aria-hidden="true"></i>
+	New</a></li>
+<li><a <?php echo $nav ?> href="#">
+	<i class="fa fa-trash fa-2x" aria-hidden="true"></i>
+	Delete</a></li>
+<li><a href="#">
+	<i class="fa fa-print fa-2x" aria-hidden="true"></i>
+	Print</a></li>
+<li><a href="#">
+	<i class="fa fa-file-excel-o fa-2x" aria-hidden="true"></i>
+	Excel</a></li>
+<li><a href="#">
+	<i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
+	Letter</a></li>
+<li><a href="#">
+	<i class="fa fa-comments fa-2x" aria-hidden="true"></i>
+	Sms</a></li>
+<li><a href="#">
+	<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
+	Email</a></li>
+<li><a href="#">
+	<i class="fa fa-refresh fa-2x fa-spin" aria-hidden="true"></i>
+	Refresh</a></li>
+</ul>
