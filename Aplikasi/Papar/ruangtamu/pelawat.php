c541dac1fc37487530069358eb75d7be19b78400
amin007 <helmi007@yahoo.com>
2017-10-02 18:22:49 +0800

    paparan untuk pelawat

--- Aplikasi/Papar/ruangtamu/pelawat.php
+++ Aplikasi/Papar/ruangtamu/pelawat.php
@@ -1,25 +1,26 @@
 <div class="container">
 <hr><h1>Ruangtamu - Untuk Pelawat</h1><hr>
+<?php include 'menu_teks.php'; ?>
 <div class="hero-unit">
-<?php butangHantar(); ?>
-<a class="btn btn-primary btn-large" href="<?php echo URL ?>ruangtamu/logout">Keluar<i class="fa fa-binoculars fa-2x"></i></a>
+<?php jadual($this->senarai); ?>
 
 </div><!-- / class="hero-unit" -->
 </div><!-- / class="container" -->
 
 <?php
-function butangHantar($_jadual = null, $cariID = 'paid')
+function jadual($senarai)
 {
-	$_jadual = 'kursus_php';
-	$_jadual = 'msic2008';
-	$_jadual = 'mcpa2009_tr2015';
-	//$cetakXml = URL . 'dataxml/papar/' . $_jadual . '/' . $cariID;
-	$cetakXml = URL . 'dataxml/paparXml/' . $_jadual . '/' . $cariID;
-	$cetakJson = URL . 'datajson/papar/' . $_jadual . '/' . $cariID;
-	$tempahan = URL . 'tempahan/mohon';
-	$_jadual = ''; ?>
-	<a target="_blank" href="<?php echo $cetakXml ?>" class="btn btn-warning btn-large">Cetak Xml</a>
-	<a target="_blank" href="<?php echo $cetakJson ?>" class="btn btn-success btn-large">Cetak Json</a>
-	<a target="_blank" href="<?php echo $tempahan ?>" class="btn btn-info btn-large">Tempahan</a>
+	foreach ($senarai as $myTable => $row)
+	{
+		if ( count($row)==0 )	echo '';
+		else
+		{
+	?>
+	<!-- Jadual <?php echo $myTable ?> ########################################### -->
+	<h2><?php echo $myTable ?></h2>
+	<?php include 'papar_jadual_modul.php'; ?>
+	<!-- Jadual <?php echo $myTable ?> ########################################### -->
 	<?php
+		} # if ( count($row)==0 )
+	} //*/
 }
