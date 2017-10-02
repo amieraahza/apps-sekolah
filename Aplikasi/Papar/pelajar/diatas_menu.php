cbe54f4945a792a74f14e958d92999c615140760
amin007 <helmi007@yahoo.com>
2017-10-02 16:51:50 +0800

    alihkan menu ke tempat lain

--- Aplikasi/Papar/pelajar/diatas_menu.php
+++ Aplikasi/Papar/pelajar/diatas_menu.php
@@ -1,33 +1,8 @@
-<ul class="nav nav-pills">
-<li><a href="#">
-	<i class="fa fa-file-o fa-2x" aria-hidden="true"></i>
-	New</a></li>
-<li><a <?php echo $nav ?> href="#">
-	<i class="fa fa-trash fa-2x" aria-hidden="true"></i>
-	Delete</a></li>
-<li><a href="#">
-	<i class="fa fa-print fa-2x" aria-hidden="true"></i>
-	Print</a></li>
-<li><a href="<?php echo URL ?>pelajar/paparBiodata">
-	<i class="fa fa-table fa-2x" aria-hidden="true"></i>
-	Table</a></li>
-<li><a href="<?php echo URL ?>pelajar/paparBiodata">
-	<i class="fa fa-file-excel-o fa-2x" aria-hidden="true"></i>
-	Excel</a></li>
-<li><a href="<?php echo URL ?>surat/buatsurat">
-	<i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i>
-	Letter</a></li>
-<li><a href="#">
-	<i class="fa fa-comments fa-2x" aria-hidden="true"></i>
-	Sms</a></li>
-<li><a href="#">
-	<i class="fa fa-envelope fa-2x" aria-hidden="true"></i>
-	Email</a></li>
-<li><a href="#">
-	<i class="fa fa-refresh fa-2x fa-spin" aria-hidden="true"></i>
-	Refresh</a></li>
+<ul class="nav nav-tabs">
+<li role="presentation"><a href="<?php echo URL ?>pelajar/paparBiodata">
+	Registration Proses</a></li>
+<li role="presentation"><a href="#">
+	Interview Process</a></li>
+<li role="presentation"><a href="<?php echo URL ?>pelajar/laporanDaftar">
+	Registration Report</a></li>
 </ul>
-<?php 
-if (isset($this->jenisBorang)):
-else: include 'drop_menu.php';
-endif;
