94f650443ce85ca131672866286ad59d2584081a
amin007 <helmi007@yahoo.com>
2017-10-04 11:05:19 +0800

    bina paparKandungan($folder, $fail) , semakPembolehubah() dan namaClass()

--- Aplikasi/Kawal/login.php
+++ Aplikasi/Kawal/login.php
@@ -3,12 +3,12 @@ namespace Aplikasi\Kawal; //echo __NAMESPACE__;
 class Login extends \Aplikasi\Kitab\Kawal
 {
 #==========================================================================================
-	function __construct() 
+	function __construct()
 	{
 		//echo '<br>class Index extends Kawal';
 		parent::__construct();
 		//\Aplikasi\Kitab\Kebenaran::kawalMasuk();
-		$this->_folder = 'login';
+		$this->_folder = huruf('kecil', namaClass($this));
 	}
 #==========================================================================================
 	public function index()
@@ -17,12 +17,26 @@ class Login extends \Aplikasi\Kitab\Kawal
 		$this->papar->menuatas = 'tak';
 		$this->papar->TajukBesar = 'Sila Login';
 
-		# pergi papar kandungan
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan($this->_folder, 'index');
+	}
+
+	public function paparKandungan($folder, $fail)
+	{	# Pergi papar kandungan
 		$jenis = $this->papar->pilihTemplate($template=0);
 		$this->papar->bacaTemplate(
 		//$this->papar->paparTemplate(
-		'login/index',$jenis,0); # $noInclude=0 
+			$folder . '/' . $fail, $jenis, 0); # $noInclude=0
+			//'mobile/mobile',$jenis,0); # $noInclude=0
 		//*/
+	}
+
+	public function semakPembolehubah($senarai)
+	{
+		echo '<pre>$senarai:<br>';
+		print_r($senarai);
+		echo '</pre>|';//*/
 	}
 #==========================================================================================
 	function semakid()
@@ -46,12 +60,9 @@ class Login extends \Aplikasi\Kitab\Kawal
 		$this->papar->sesat = 'Enjin Carian - Sesat';
 		$this->papar->isi = '';
 
-		# pergi papar kandungan
-		$jenis = $this->papar->pilihTemplate($template=0);
-		$this->papar->bacaTemplate(
-		//$this->papar->paparTemplate(
-			'index/salah',$jenis,0); # $noInclude=0
-		//*/
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index', 'salah');
 	}
 #==========================================================================================
 	function semaknama($nama)
