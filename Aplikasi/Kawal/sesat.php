753e14b782b2239e8640280f636cae758ad75468
amin007 <helmi007@yahoo.com>
2017-10-04 11:01:02 +0800

    bina paparKandungan() , semakPembolehubah() dan namaClass()

--- Aplikasi/Kawal/sesat.php
+++ Aplikasi/Kawal/sesat.php
@@ -7,29 +7,42 @@ class Sesat extends \Aplikasi\Kitab\Kawal
 	{
 		parent::__construct();
 		$this->_tajukAtas = 'Enjin - Sesat';
-		$this->_folder = 'sesat';
+		$this->_folder = huruf('kecil', namaClass($this));
 	}
 
 	function index() 
 	{
 		$this->papar->mesej = 'Halaman ini tidak wujud';
 
-		# pergi papar kandungan
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index');
+	}
+
+	public function paparKandungan($fail)
+	{	# Pergi papar kandungan
 		$jenis = $this->papar->pilihTemplate($template=0);
 		$this->papar->bacaTemplate(
 		//$this->papar->paparTemplate(
-			$this->_folder . '/index', $jenis, 0); # $noInclude=0
+			$this->_folder . '/' . $fail, $jenis, 0); # $noInclude=0
+			//'mobile/mobile',$jenis,0); # $noInclude=0
+		//*/
 	}
 
+	public function semakPembolehubah($senarai)
+	{
+		echo '<pre>$senarai:<br>';
+		print_r($senarai);
+		echo '</pre>|';//*/
+	}
+#==========================================================================================
 	function parameter()
 	{
 		$this->papar->mesej = 'Class wujud tapi parameter/method/fungsi tidak wujud';
 
-		# pergi papar kandungan
-		$jenis = $this->papar->pilihTemplate($template=0);
-		$this->papar->bacaTemplate(
-		//$this->papar->paparTemplate(
-			$this->_folder . '/index', $jenis, 0); # $noInclude=0
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index');
 	}
 
 	function classTidakWujud($amaran)
@@ -37,11 +50,9 @@ class Sesat extends \Aplikasi\Kitab\Kawal
 		$this->papar->mesej = $amaran;
 		$this->papar->Tajuk_Muka_Surat = $this->_tajukAtas . $this->papar->mesej;
 
-		# pergi papar kandungan
-		$jenis = $this->papar->pilihTemplate($template=0);
-		$this->papar->bacaTemplate(
-		//$this->papar->paparTemplate(
-			$this->_folder . '/index', $jenis, 0); # $noInclude=0
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index');
 	}
 
 	function methodTanyaTidakWujud($amaran,$class,$method)
@@ -50,11 +61,9 @@ class Sesat extends \Aplikasi\Kitab\Kawal
 			. "|class=$class|fungsi=$method|";
 		$this->papar->Tajuk_Muka_Surat = $this->_tajukAtas . $this->papar->mesej;
 
-		# pergi papar kandungan
-		$jenis = $this->papar->pilihTemplate($template=0);
-		$this->papar->bacaTemplate(
-		//$this->papar->paparTemplate(
-			$this->_folder . '/index', $jenis, 0); # $noInclude=0
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index');
 	}
 
 	function folderPaparTidakWujud() 
@@ -62,12 +71,9 @@ class Sesat extends \Aplikasi\Kitab\Kawal
 		echo $this->papar->mesej = 'folder tidak wujud dalam PAPAR';
 		$this->papar->Tajuk_Muka_Surat = $this->_tajukAtas . $this->papar->mesej;
 
-		# pergi papar kandungan
-		$jenis = $this->papar->pilihTemplate($template=0);
-		//$this->papar->bacaTemplate(
-		$this->papar->paparTemplate(
-			$this->_folder . '/index', $jenis, 0); # $noInclude=0
-		//*/
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index');
 	}
 
 	function failTidakWujud() 
@@ -75,11 +81,9 @@ class Sesat extends \Aplikasi\Kitab\Kawal
 		$this->papar->mesej = 'Fail tidak wujud dalam PAPAR';
 		$this->papar->Tajuk_Muka_Surat = $this->_tajukAtas . $this->papar->mesej;
 
-		# pergi papar kandungan
-		$jenis = $this->papar->pilihTemplate($template=0);
-		$this->papar->bacaTemplate(
-		//$this->papar->paparTemplate(
-			$this->_folder . '/index', $jenis, 0); # $noInclude=0
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index');
 	}
 
 	function masalahDB($amaran)
@@ -87,11 +91,9 @@ class Sesat extends \Aplikasi\Kitab\Kawal
 		$this->papar->mesej = $amaran;
 		$this->papar->Tajuk_Muka_Surat = $this->_tajukAtas . $this->papar->mesej;
 
-		# pergi papar kandungan
-		$jenis = $this->papar->pilihTemplate($template=0);
-		$this->papar->bacaTemplate(
-		//$this->papar->paparTemplate(
-			$this->_folder . '/index', $jenis, 0); # $noInclude=0
+		# Pergi papar kandungan
+		//$this->semakPembolehubah($this->papar->mesej); # Semak data dulu
+		$this->paparKandungan('index');
 	}
 #==================================================================================================================
 }
