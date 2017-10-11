1e36cdabc6c5463e751d59846569ecfbd27c5c1c
amin007 <helmi007@yahoo.com>
2017-10-11 16:57:07 +0800

    bina Summary(), kodSummary3(), kodSummary4()

--- Aplikasi/Kitab/Vendor/Borang/Jquery_Script.php
+++ Aplikasi/Kitab/Vendor/Borang/Jquery_Script.php
@@ -4,6 +4,32 @@ class Jquery_Script
 {
 #==========================================================================================
 #------------------------------------------------------------------------------------------
+	public static function Summary()
+	{
+?>jQuery.noConflict();
+var example = 'column-parsed', 
+theme = 'default';
+(function($){ // encapsulate jQuery
+	Highcharts.chart('container', 
+	{
+		data: { table: 'datatable' },
+		chart:{ type: 'column' },
+		title:{ text: 'Data dari jadual sedia ada' },
+		yAxis:{
+			allowDecimals: false,
+			title: { text: 'Marks' }
+		},
+		tooltip: {
+			formatter: function () {
+				return '<b>' + this.series.name + '</b><br/>' +
+				this.point.y + ' ' + this.point.name.toLowerCase();
+			}
+		}
+	});	
+})(jQuery);
+<?php
+	}
+#------------------------------------------------------------------------------------------
 	public static function kodSummary1($namaTajukGraf)
 	{
 ?>
@@ -26,7 +52,7 @@ theme = 'default';
 				this.point.y + ' ' + this.point.name.toLowerCase();
 			}
 		}
-	});	
+	});
 })(jQuery);
 <?php
 	}
@@ -87,8 +113,122 @@ theme = 'default';
 				style: { fontSize: '13px', fontFamily: 'Verdana, sans-serif' }
 			}
 		}]
-	});		
+	});
 })(jQuery);
+<?php
+	}
+#------------------------------------------------------------------------------------------
+	public static function kodSummary3()
+	{
+?>/* Kod Summary3 */
+jQuery.noConflict();
+var example = 'column-rotated-labels', 
+theme = 'default';
+(function($){ // encapsulate jQuery
+	Highcharts.chart('kontena3', 
+	{
+		chart: { type: 'column' },
+		title: { text: 'peperiksaan : analisa subjek' },
+		xAxis: {
+			type: 'category',
+			labels: {
+				rotation: 45,
+				style: { fontSize: '13px', fontFamily: 'Verdana, sans-serif' }
+			}
+		},
+		yAxis: { min: 0, title: { text: 'Markah Purata - 2016' } },
+		legend: { enabled: false  },
+		tooltip: { pointFormat: '<b>{point.y:.1f} %</b>' },
+		series: [{
+			name: 'mata pelajaran',
+			data: [
+					['bahasa arab', 77],
+					['bahasa inggeris bertulis', 84],
+					['bahasa inggeris lisan', 0],
+					['bahasa melayu bertulis', 67],
+					['bahasa melayu lisan', 0],
+					['geografi', 67],
+					['ict', 66],
+					['kemahiran hidup bersepadu 1', 70],
+					['kemahiran hidup bersepadu 2', 60],
+					['matematik', 42],
+					['pendidikan islam', 84],
+					['pendidikan jasmani dan kesihatan', 0],
+					['sains', 65],
+					['sejarah', 52]
+				],
+			dataLabels: {
+				enabled: true,
+				rotation: 0,
+				color: '#FFFFFF',
+				align: 'right',
+				format: '{point.y:.1f}', // one decimal
+				y: -5, // 10 pixels down from the top
+				style: { fontSize: '13px', fontFamily: 'Verdana, sans-serif' }
+			}
+		}]
+	});
+})(jQuery);
+<?php
+	}
+#------------------------------------------------------------------------------------------
+	public static function kodSummary4()
+	{
+?>/* Kod Summary4 */
+jQuery.noConflict();
+var example = 'column-rotated-labels', 
+theme = 'default';
+(function($){ // encapsulate jQuery
+	Highcharts.chart('kontena4', 
+	{
+		chart: { type: 'column' },
+		title: { text: 'Peperiksaan : Analisa Keseluruhan'},
+		subtitle: {	text: 'Pentafsiran Sumatif' },
+		xAxis: {
+			categories: [
+				'bahasa arab',
+				'bahasa inggeris bertulis', 
+				'bahasa inggeris lisan',
+				'bahasa melayu bertulis',
+				'bahasa melayu lisan',
+				'geografi',
+				'ict',
+				'kemahiran hidup bersepadu 1',
+				'kemahiran hidup bersepadu 2',
+				'matematik',
+				'pendidikan islam',
+				'pendidikan jasmani dan kesihatan',
+				'sains',
+				'sejarah'
+			],
+			crosshair: true,
+			labels: { rotation: 60 }
+		},
+		yAxis: {min: 0, title: { text: 'Markah Purata 2016' } },
+		tooltip: {
+			backgroundColor: '#FCFFC5',
+			headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+			pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+				'<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+			footerFormat: '</table>',
+			shared: true,
+			useHTML: true
+		},
+		plotOptions: { column: { pointPadding: 0.2, borderWidth: 0 } },
+		series: [{
+				name:'Sumatif 1', data:[60,82,33, 75,0,74, 60,65,58, 52,74,0, 64,53],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+			},{
+				name:'Sumatif 2', data:[0,0,0, 0,0,0, 0,0,0, 0,80,0, 0,0],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+			},{ 
+				name:'Sumatif 3', data: [0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+		}]
+
+	});
+})(jQuery);
+	
 <?php
 	}
 #------------------------------------------------------------------------------------------
