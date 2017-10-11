7a8e3df61ca71c07d785b0fa452afd643aac3469
amin007 <helmi007@yahoo.com>
2017-10-11 15:19:14 +0800

    buat bootstrap tabbable

--- Aplikasi/Papar/pelajar/laporan_peperiksaan_subjek.php
+++ Aplikasi/Papar/pelajar/laporan_peperiksaan_subjek.php
@@ -5,33 +5,50 @@ $html = new \Aplikasi\Kitab\HTML_Table();
 $classTable = 'table table-condensed';
 $header = null;
 ?>
-<div class="container">
+
+<div class="tabbable tabs-top">
+	<ul class="nav nav-tabs putih">
+	
+		<li><a href="#kelas" data-toggle="tab">
+		<span class="badge badge-success">kelas</span>
+		</a></li>
+		<li><a href="#graf" data-toggle="tab">
+		<span class="badge badge-success">graf</span>
+		</a></li>	</ul>
+	<div class="tab-content">
+		<div class="tab-pane" id="kelas">
 <!-- ********************************************************************* -->
+<div class="container">
 	<div class="row">
 		<div class="col-sm-8">
 			<div class="row">
 				<div class="col-xs-6 col-sm-6">
 	<!-- ********************************************************************* --><?php
 	echo "\n\t\t";
-	$html->papar_jadual($this->senarai['Kelas'], 'Kelas', 
-	$pilih = 1, $classTable); echo "\n\t";
+	$html->papar_jadual($this->senarai['kelas'], 'Kelas', 
+	$pilih = 1, $classTable, null, 'kelas'); echo "\n\t";
 ?>	<!-- ********************************************************************* -->
 				</div>
 				<div class="col-xs-6 col-sm-6">
 	<!-- ********************************************************************* --><?php
 	echo "\n\t\t";
-	$html->papar_jadual($this->senarai['Summary'], 'Summary',
-	$pilih = '4_1', $classTable, null, 'summary'); echo "\n\t";
+	$html->papar_jadual($this->senarai['datatable'], 'datatable',
+	$pilih = '4_1', $classTable, null, 'datatable'); echo "\n\t";
 ?><!-- ********************************************************************* -->
 				</div>
 			</div>
 		</div>
 	</div>
-<!-- ********************************************************************* -->
 </div><!-- / class="container" -->
-
+<!-- ********************************************************************* -->
+		</div>
+		<div class="tab-pane" id="graf">
+<!-- ********************************************************************* -->
 <div id="container" style="min-width: 500px; height: 500px; margin: 0 auto"></div>
-
+<!-- ********************************************************************* -->
+		</div>
+	</div><!-- class="tab-content" -->
+</div><!-- /tabbable -->
 <?php
 /*# Semak data $this->senarai
 echo '<pre>$this->senarai:<br>'; 
