6016253b63b8253e9eb43da4814b6d1de14ebfc7
amin007 <helmi007@yahoo.com>
2017-10-12 11:39:27 +0800

    bina kodSummary5(), kodSummary6()

--- Aplikasi/Kitab/Vendor/Borang/Jquery_Script.php
+++ Aplikasi/Kitab/Vendor/Borang/Jquery_Script.php
@@ -225,10 +225,130 @@ theme = 'default';
 				name:'Sumatif 3', data: [0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0],
 				dataLabels: { enabled: true, rotation: 0, y: -5}
 		}]
-
 	});
 })(jQuery);
-	
+
+<?php
+	}
+#------------------------------------------------------------------------------------------
+	public static function kodSummary5()
+	{
+?>/* Kod Summary5 */
+jQuery.noConflict();
+var example = 'column-rotated-labels', 
+theme = 'default';
+(function($){ // encapsulate jQuery
+	Highcharts.chart('kontena5', 
+	{
+		chart: { type: 'column' },
+		title: { text: 'Peperiksaan : Analisa Tahunan'},
+		subtitle: {	text: '' },
+		xAxis: {
+			categories: [
+				'bahasa arab',
+				'bahasa inggeris bertulis', 
+				'bahasa inggeris lisan',
+				'bahasa melayu bertulis',
+				'bahasa melayu lisan',
+				'geografi',
+				'ict',
+				'kemahiran hidup bersepadu 1',
+				'kemahiran hidup bersepadu 2',
+				'matematik',
+				'pendidikan islam',
+				'pendidikan jasmani dan kesihatan',
+				'sains',
+				'sejarah'
+			],
+			crosshair: true,
+			labels: { rotation: 60 }
+		},
+		yAxis: {min: 0, title: { text: 'Markah Purata 2016' } },
+		tooltip: {
+			backgroundColor: '#FCFFC5',
+			headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+			pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}:</td>' +
+				'<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+			footerFormat: '</table>',
+			shared: true,
+			useHTML: true
+		},
+		plotOptions: { column: { pointPadding: 0.2, borderWidth: 0 } },
+		series: [{
+				name:'2016', data:[60,82,33, 75,0,74, 60,65,58, 52,74,0, 64,53],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+			},{
+				name:'2015', data:[0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+			},{ 
+				name:'2014', data: [0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+			},{
+				name:'2013', data:[0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+			},{ 
+				name:'2012', data: [0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+		}]
+	});
+})(jQuery);
+
+<?php
+	}
+#------------------------------------------------------------------------------------------
+	public static function kodSummary6()
+	{
+?>/* Kod Summary5 */
+jQuery.noConflict();
+var example = 'column-rotated-labels', 
+theme = 'default';
+(function($){ // encapsulate jQuery
+	Highcharts.chart('kontena6', 
+	{
+		chart: { type: 'column' },
+		title: { text: 'Peperiksaan : Analisa Prasekolah'},
+		subtitle: {	text: '' },
+		xAxis: {
+			categories: [
+				'bahasa arab',
+				'bahasa inggeris bertulis', 
+				'bahasa inggeris lisan',
+				'bahasa melayu bertulis',
+				'bahasa melayu lisan',
+				'geografi',
+				'ict',
+				'kemahiran hidup bersepadu 1',
+				'kemahiran hidup bersepadu 2',
+				'matematik',
+				'pendidikan islam',
+				'pendidikan jasmani dan kesihatan',
+				'sains',
+				'sejarah'
+			],
+			crosshair: true,
+			labels: { rotation: 60 }
+		},
+		yAxis: {min: 0, title: { text: 'Markah Purata 2016' } },
+		tooltip: {
+			backgroundColor: '#FCFFC5',
+			headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+			pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}:</td>' +
+				'<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+			footerFormat: '</table>',
+			shared: true,
+			useHTML: true
+		},
+		plotOptions: { column: { pointPadding: 0.2, borderWidth: 0 } },
+		series: [{
+				name:'SMIAA GOMBAK', data:[60,82,33, 75,0,74, 60,65,58, 52,74,0, 64,53],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+			},{
+				name:'SMAIAABM', data:[0,0,0, 0,0,0, 0,0,0, 0,0,0, 0,0],
+				dataLabels: { enabled: true, rotation: 0, y: -5}
+		}]
+	});
+})(jQuery);
+
 <?php
 	}
 #------------------------------------------------------------------------------------------
